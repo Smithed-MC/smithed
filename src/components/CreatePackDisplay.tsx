4d063fe75904aaa1a59b9143c09d1b294d682699
TheNuclearNexus <thenuclearnexus@gmail.com>
2021-11-04 12:57:14 -0400

    little more

--- src/components/CreatePackDisplay.tsx
+++ src/components/CreatePackDisplay.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import { RowDiv,} from '..';
 import { Pack } from '../Pack';
 import curPalette from '../Palette'
@@ -30,7 +29,7 @@ class CreatePackDisplay extends React.Component {
     render() {
         return(
             <RowDiv style={{backgroundColor:curPalette.darkBackground, alignItems:'center', justifyContent:'left', padding: 8, gap: 8, width:320, height:64}} onClick={this.props.onClick}>
-                <img style={{width:64,height:64, WebkitUserSelect:'none', filter: this.props.pack.display === 'hidden' ? filter : ''}} src={this.props.pack.display !== 'hidden' ? this.props.pack.display.icon : Hidden}/>
+                <img style={{width:64,height:64, WebkitUserSelect:'none', filter: this.props.pack.display === 'hidden' ? filter : ''}} src={this.props.pack.display !== 'hidden' ? this.props.pack.display.icon : Hidden} alt="Pack icon"/>
                 <StyledLabel style={{WebkitUserSelect:'none',fontFamily:'Disket-Bold', color: this.props.pack.messages != null && this.props.pack.messages.length > 0 ? 'red' : curPalette.text}}>{this.props.pack.display !== 'hidden' ? this.props.pack.display.name : this.props.pack.id}</StyledLabel>
             </RowDiv>
         );
